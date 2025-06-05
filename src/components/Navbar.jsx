872e82d2f22244a170278c9cf766e27dbd50f8ee
Cy Sepagan <cy.sepagan07@gmail.com>
2025-06-05 18:26:38 +0800

    Setup basic topbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
+import Logo from "../assets/images/logo.png"
+const navLinks = [
+    { title: "Home", path: "/"},
+    { title: "Projects", path: "/projects"}
+];
 
-
-function Navbar() {
+const Navbar = () => {
     return (
-        <nav>
-            <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/projects">Projects</a></li>
-            </ul>
-        </nav>
+        <header className="container mx-auto flex flex-row py-4">
+            <span className="flex-1"><img src={Logo} alt="personal logo" /></span>
+            <nav>
+                <ul className="flex flex-row flex-1">
+                    {navLinks.map((link) => (
+                        <li><a href={link.path} className="px-5 py-5">{link.title}</a></li>
+                    ))}
+                </ul>
+            </nav>
+        </header>        
+        
     );
 }
 
